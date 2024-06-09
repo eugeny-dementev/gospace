cf0d867eaca95c84670089da39c7de0b728e0c2d
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-09 18:00:53 +0800

    request albums by name

--- database/main.go
+++ database/main.go
@@ -32,4 +32,42 @@ func main() {
   }
 
   fmt.Println("Connected", cfg.FormatDSN())
+
+  albums, err := albumsByArtist("John Coltrane")
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Printf("Albums found %v\n", albums)
+}
+
+type Album struct {
+	Title  string
+	Artist string
+	ID     int64
+	Price  float32
+}
+
+func albumsByArtist(name string) ([]Album, error) {
+  var albums []Album
+
+  rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+  if err != nil {
+    return nil, fmt.Errorf("albumsByArtists %q, %v", name, err)
+  }
+
+  defer rows.Close()
+
+  for rows.Next() {
+    var alb Album
+    if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+      return nil, fmt.Errorf("albumsByArtist %q, %v", name, err)
+    }
+    albums = append(albums, alb)
+  }
+
+  if err := rows.Err(); err != nil {
+    return nil, fmt.Errorf("albumsByArtist %q, %v", name, err)
+  }
+
+  return albums, nil
 }
