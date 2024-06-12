e010a0ca37df698ef42e4fb805d77454b6cd0639
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-12 14:21:53 +0800

    feat: setup first endpoint to return all albums

--- api/main.go
+++ api/main.go
@@ -3,13 +3,33 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"example.com/database"
+	"github.com/gin-gonic/gin"
 )
+
+func handlerFactory(db *database.AlbumsDB) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		albums, err := db.Albums()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Albums found %v\n", albums)
+		c.IndentedJSON(http.StatusOK, albums)
+	}
+}
 
 func main() {
 	albumsDB := database.AlbumsDB{}
 	albumsDB.Connect()
+
+	getAlbumsHandler := handlerFactory(&albumsDB)
+
+	router := gin.Default()
+	router.GET("/", getAlbumsHandler)
+
+	router.Run("localhost:8888")
 
 	albums, err := albumsDB.AlbumsByArtist("John Coltrane")
 	if err != nil {
