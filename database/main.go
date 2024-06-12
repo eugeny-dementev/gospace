aad94ac58c72ef7ecc1ebfe78a884fbd920e6f05
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-12 13:57:16 +0800

    feat: move main() out of the way

--- database/main.go
+++ database/main.go
@@ -12,7 +12,7 @@ import (
 var db *sql.DB
 
 type AlbumsDB struct {
-  db *sql.DB
+	db *sql.DB
 }
 
 func (c *AlbumsDB) Connect() error {
@@ -37,7 +37,7 @@ func (c *AlbumsDB) Connect() error {
 
 	fmt.Println("Connected", cfg.FormatDSN())
 
-  return nil
+	return nil
 }
 
 type Album struct {
@@ -45,52 +45,6 @@ type Album struct {
 	Artist string
 	ID     int64
 	Price  float32
-}
-
-func main() {
-	cfg := mysql.Config{
-		User:   os.Getenv("DBUSER"),
-		Passwd: os.Getenv("DBPASS"),
-		Net:    "tcp",
-		Addr:   "127.0.0.1:3306",
-		DBName: "recordings",
-	}
-
-	var err error
-	db, err = sql.Open("mysql", cfg.FormatDSN())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pingErr := db.Ping()
-	if pingErr != nil {
-		log.Fatal(pingErr)
-	}
-
-	fmt.Println("Connected", cfg.FormatDSN())
-
-	albums, err := AlbumsByArtist("John Coltrane")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Albums found %v\n", albums)
-
-	album, err := AlbumByID(1)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Found album:", album)
-
-	newAlbum := Album{
-		Title:  "The Modern Sound of Betty Carter",
-		Artist: "Betty Carter",
-		Price:  49.99,
-	}
-	albumId, err := AddAlbum(newAlbum)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("ID of new album: %v", albumId)
 }
 
 func AlbumsByArtist(name string) ([]Album, error) {
@@ -144,4 +98,50 @@ func AddAlbum(album Album) (int64, error) {
 	}
 
 	return id, nil
+}
+
+func main() {
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Println("Connected", cfg.FormatDSN())
+
+	albums, err := AlbumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found %v\n", albums)
+
+	album, err := AlbumByID(1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Found album:", album)
+
+	newAlbum := Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	}
+	albumId, err := AddAlbum(newAlbum)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID of new album: %v", albumId)
 }
