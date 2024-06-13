432ba78ea99604712b880245bd9bbc24d08cf780
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-13 23:18:31 +0800

    throw an error if input is not valid utf-8

--- fuzz/main.go
+++ fuzz/main.go
@@ -1,21 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"unicode/utf8"
+)
 
 func main() {
-	input := "The quick brown fox jumped over the lazy dog"
-	rev := Reverse(input)
-	doubleRev := Reverse(rev)
+	input := "\xff"
+	rev, err := Reverse(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	doubleRev, err := Reverse(rev)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println(input)
 	fmt.Println(rev)
 	fmt.Println(doubleRev)
 }
 
-func Reverse(s string) string {
+func Reverse(s string) (string, error) {
+	if !utf8.ValidString(s) {
+		return s, errors.New("Input is not valid UTF-8 string")
+	}
+
 	b := []rune(s)
 	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
 		b[i], b[j] = b[j], b[i]
 	}
-	return string(b)
+	return string(b), nil
 }
