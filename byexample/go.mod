afe30d7a774d1c6e7e9a2d4a76823671225323c8
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-14 20:51:35 +0800

    go mod add constraints

--- byexample/go.mod
+++ byexample/go.mod
@@ -1,3 +1,6 @@
 module example.com/byexample
 
 go 1.22.3
+
+require golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8
+
--- byexample/main.go
+++ byexample/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+	"golang.org/x/exp/constraints"
 
 func main() {
 	fmt.Println("Hello from by example")
