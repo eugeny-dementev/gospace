5bfed5ede096a29816d14b03985ba809d5417f4e
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-14 20:51:51 +0800

    some experiments with slices

--- byexample/main.go
+++ byexample/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
 	"golang.org/x/exp/constraints"
+)
 
 func main() {
-	fmt.Println("Hello from by example")
+	slices()
+}
+
+func slices() {
+	var a []int
+	fmt.Println("uninit:", a, a == nil, len(a))
+
+	a = []int{1, 2, 3, 4}
+	fmt.Println("uninit:", a, a == nil, len(a))
+
+	a = make([]int, 10)
+	a[5] = 5
+	fmt.Println("uninit:", a, a == nil, len(a))
+
+	a = a[5:]
+	b := clone(a)
+	fmt.Println(a, b)
+	a[2] = 3
+	fmt.Println(a, b)
+}
+
+func clone[T constraints.Integer](a []T) []T {
+	b := make([]T, len(a))
+	copy(b, a)
+	return b
 }
