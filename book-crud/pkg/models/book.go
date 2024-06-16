9f4e1ade9af92d74aaa498717041e6cf0c858a4f
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-16 20:35:03 +0800

    preapre book model

--- book-crud/pkg/models/book.go
+++ book-crud/pkg/models/book.go
@@ -1,13 +1,27 @@
 package models
 
-import "fmt"
+import (
+	"fmt"
+
+	"example.com/book-crud/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
 
 type Book struct {
-	Name   string `json:"name"`
-	Author string `json:"author"`
-	ID     uint32 `json:"id"`
+	gorm.Model
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
 }
 
 func (b *Book) String() string {
 	return fmt.Sprintf("(%v): %v - %v", b.ID, b.Author, b.Name)
+}
+
+func init() {
+  config.Connect()
+  db = config.GetDB()
+  db.AutoMigrate(&Book{})
 }
