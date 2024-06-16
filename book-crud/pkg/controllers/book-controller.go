c4273d8da08bb5685d27f0fd2e66893a9e46ff63
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-16 21:27:32 +0800

    fix func name

--- book-crud/pkg/controllers/book-controller.go
+++ book-crud/pkg/controllers/book-controller.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"example.com/book-crud/pkg/models"
+	"example.com/book-crud/pkg/utils"
 	"github.com/gorilla/mux"
 )
 
@@ -28,16 +29,51 @@ func GetBooks(w http.ResponseWriter, req *http.Request) {
 func GetBookById(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
 	id, err := strconv.ParseInt(params["bookId"], 0, 0)
-  if err != nil {
-    log.Fatal(err)
-  }
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	book, _ := models.GetBookById(uint(id))
 
-  res, err := json.Marshal(book)
+	res, err := json.Marshal(book)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, req *http.Request) {
+	book := &models.Book{}
+	utils.ParseBody(req.Body, &book)
+
+	b := book.CreateBook()
+
+	res, err := json.Marshal(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBookById(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	id, err := strconv.ParseInt(params["bookId"], 0, 0)
   if err != nil {
     log.Fatal(err)
   }
+
+  book := models.DeleteBook(uint(id))
+
+	res, err := json.Marshal(book)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
