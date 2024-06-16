82b9bda971727f445e0bb15773c06c668ea7056d
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-16 21:16:42 +0800

    get book by id handler

--- book-crud/pkg/controllers/book-controller.go
+++ book-crud/pkg/controllers/book-controller.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"example.com/book-crud/pkg/models"
+	"github.com/gorilla/mux"
 )
 
 var NewBook models.Book
@@ -17,6 +19,25 @@ func GetBooks(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	id, err := strconv.ParseInt(params["bookId"], 0, 0)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+	book, _ := models.GetBookById(uint(id))
+
+  res, err := json.Marshal(book)
+  if err != nil {
+    log.Fatal(err)
+  }
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
