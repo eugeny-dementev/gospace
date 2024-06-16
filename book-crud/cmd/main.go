4954ce3ef826b082015b1f3e4e6fddb215ec4d32
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-16 20:54:47 +0800

    cmd run server

--- book-crud/cmd/main.go
+++ book-crud/cmd/main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"example.com/book-crud/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
 func main() {
+	router := mux.NewRouter()
+	routes.RegisterBookRoutes(router)
+	http.Handle("/", router)
+	log.Fatal(http.ListenAndServe("127.0.0.1:3007", router))
 }
