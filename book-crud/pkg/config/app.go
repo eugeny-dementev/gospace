73f436e23b44fa0f7339d52a5a136aa4462aeab8
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-16 20:16:54 +0800

    setup db connection

--- book-crud/pkg/config/app.go
+++ book-crud/pkg/config/app.go
@@ -1,1 +1,34 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+func Connect() {
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+
+	database, err := gorm.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = database
+
+	fmt.Println("Connected", cfg.FormatDSN())
+}
+
+func GetDB() *gorm.DB {
+  return db
+}
