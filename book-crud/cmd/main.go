97b4d66e84ee6391ec05f6db535a400247aa8b7d
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-16 22:30:33 +0800

    print when server is started

--- book-crud/cmd/main.go
+++ book-crud/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -12,5 +13,6 @@ func main() {
 	router := mux.NewRouter()
 	routes.RegisterBookRoutes(router)
 	http.Handle("/", router)
+	fmt.Println("Server starts listening on 3007")
 	log.Fatal(http.ListenAndServe("127.0.0.1:3007", router))
 }
