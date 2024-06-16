860185536d0f3386ace2c8209e7afc11102e66e7
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-16 21:10:22 +0800

    get books handler

--- book-crud/pkg/controllers/book-controller.go
+++ book-crud/pkg/controllers/book-controller.go
@@ -1,7 +1,24 @@
 package controllers
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
 
-func Print() {
-	fmt.Println("Hello from controllers")
+	"example.com/book-crud/pkg/models"
+)
+
+var NewBook models.Book
+
+func GetBooks(w http.ResponseWriter, req *http.Request) {
+	books := models.GetAllBooks()
+
+	res, err := json.Marshal(books)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
