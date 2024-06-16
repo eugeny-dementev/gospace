403c9dc04afaa22641934ad457c27d54bbb7a4b9
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-16 20:24:51 +0800

    book utils

--- book-crud/pkg/utils/utils.go
+++ book-crud/pkg/utils/utils.go
@@ -1,1 +1,19 @@
 package utils
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+)
+
+func ParseBody(raw io.ReadCloser, x any) {
+	body, err := io.ReadAll(raw)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(body, x)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
