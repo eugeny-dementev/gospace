aa4d4e5fcf21b9d443cfa91918f7d2658c91a8c2
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-16 22:30:53 +0800

    update route path

--- book-crud/pkg/routes/book-routes.go
+++ book-crud/pkg/routes/book-routes.go
@@ -6,8 +6,8 @@ import (
 )
 
 var RegisterBookRoutes = func(router *mux.Router) {
-  router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
-  router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
+  router.HandleFunc("/book", controllers.CreateBook).Methods("POST")
+  router.HandleFunc("/book", controllers.GetBooks).Methods("GET")
   router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
   router.HandleFunc("/book/{bookId}", controllers.UpdateBookById).Methods("PUT")
   router.HandleFunc("/book/{bookId}", controllers.DeleteBookById).Methods("DELETE")
