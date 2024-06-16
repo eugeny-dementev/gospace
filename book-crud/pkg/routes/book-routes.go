207428db1b87b473ea9630ee0a2e435c7ed08faa
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-16 20:17:05 +0800

    prepare routes

--- book-crud/pkg/routes/book-routes.go
+++ book-crud/pkg/routes/book-routes.go
@@ -1,10 +1,14 @@
 package routes
 
 import (
-  "example.com/book-crud/pkg/controllers"
+	"example.com/book-crud/pkg/controllers"
+	"github.com/gorilla/mux"
 )
 
-func main() {
-  controllers.Print()
+var RegisterBookRoutes = func(router *mux.Router) {
+  router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+  router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
+  router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+  router.HandleFunc("/book/{bookId}", controllers.UpdateBookById).Methods("PUT")
+  router.HandleFunc("/book/{bookId}", controllers.DeleteBookById).Methods("DELETE")
 }
-
