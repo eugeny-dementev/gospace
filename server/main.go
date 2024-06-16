5d6d7cb97f76d10fe077ac1f12d08a91ec8a75f2
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-16 15:02:18 +0800

    setup few endpoints

--- server/main.go
+++ server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	log.Print("Hello from server")
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+  log.Print("Starting server at port 8082")
+  if err := http.ListenAndServe(":8082", nil); err != nil {
+    log.Fatal(err)
+  }
+}
+
+func formHandler(w http.ResponseWriter, req *http.Request) {
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
 }
