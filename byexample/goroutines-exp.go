79c95a3ab7f0e932fa2238603dc3179acd48632e
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-26 22:47:35 +0800

    math/rand.Intn

--- byexample/goroutines-exp.go
+++ byexample/goroutines-exp.go
@@ -2,26 +2,31 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 func goroutinesExp() {
 	fmt.Println("Goroutines experiments")
 
-  gof("direct")
-  go gof("goroutine")
-  go func(msg string) {
-    time.Sleep(time.Millisecond)
-    fmt.Println(msg)
-  }("going")
+	gof("direct")
+	go gof("goroutine")
+	go func(msg string) {
+		SleepRand(100)
+		fmt.Println(msg)
+	}("going")
 
-  time.Sleep(time.Second)
-  fmt.Println("done")
+	time.Sleep(time.Second)
+	fmt.Println("done")
 }
 
 func gof(from string) {
-  for i := 0; i < 3; i++ {
-    time.Sleep(time.Millisecond)
-    fmt.Println(from, ":", i)
-  }
+	for i := 0; i < 3; i++ {
+		SleepRand(50)
+		fmt.Println(from, ":", i)
+	}
+}
+
+func SleepRand(n int) {
+	time.Sleep(time.Millisecond * time.Duration((rand.Intn(n))))
 }
