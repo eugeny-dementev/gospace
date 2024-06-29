2c48ad5e4f1f7f670e7c07818378d964fc46aeac
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-29 20:49:47 +0800

    feat: channels sync experiment

--- byexample/channels-exp.go
+++ byexample/channels-exp.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func channelsExp() {
 	message := make(chan string)
@@ -8,4 +11,18 @@ func channelsExp() {
 		message <- "hello world"
 	}()
   fmt.Println("Message:", <- message)
+
+  done := make(chan bool)
+  go worker(5, done)
+
+  <- done
+  fmt.Println("All workers finished")
+}
+
+func worker(id int, done chan bool) {
+  fmt.Printf("worker %d is working\n", id)
+  time.Sleep(time.Millisecond * 10)
+  fmt.Printf("worker %d is done\n", id)
+
+  done <- true
 }
