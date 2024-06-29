09a0500d13b290d59a7591f3b4b5d8a8d783376c
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-29 20:58:42 +0800

    feat: extend workers sync example

--- byexample/channels-exp.go
+++ byexample/channels-exp.go
@@ -10,19 +10,33 @@ func channelsExp() {
 	go func() {
 		message <- "hello world"
 	}()
-  fmt.Println("Message:", <- message)
+	fmt.Println("Message:", <-message)
 
-  done := make(chan bool)
-  go worker(5, done)
+	done := make(chan bool)
 
-  <- done
-  fmt.Println("All workers finished")
+	list := []int{1, 2, 3, 4, 5}
+	doneList := make([]int, 5)
+	for _, v := range list {
+		go worker(v, done)
+	}
+
+	for v := range len(list) {
+		<-done
+		doneList[v] = 1
+	}
+
+	for _, v := range doneList {
+		if v != 1 {
+			panic("not all workers finished")
+		}
+	}
+	fmt.Println("All workers finished", len(doneList))
 }
 
 func worker(id int, done chan bool) {
-  fmt.Printf("worker %d is working\n", id)
-  time.Sleep(time.Millisecond * 10)
-  fmt.Printf("worker %d is done\n", id)
+	fmt.Printf("worker %d is working\n", id)
+	time.Sleep(time.Millisecond * 10)
+	fmt.Printf("worker %d is done\n", id)
 
-  done <- true
+	done <- true
 }
